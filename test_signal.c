ee1ebff65a76786c3003a90cb09d1e5422378b59
Asaiba <A.saiba@alustudent.com>
2025-06-27 11:04:36 +0200

    done

--- test_signal.c
+++ test_signal.c
@@ -1,28 +1,24 @@
-// File: test_signal.c
 #include <stdio.h>
-#include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-// Handler for SIGTSTP (Ctrl+Z)
-void handle_sigint(int sig) {
-    printf("\nReceived SIGTINT (Ctrl+C). Exited...\n");
-    exit(0);
+// This function runs when SIGTSTP (Ctrl+Z) is received
+void handle_SIGTSTP(int sig) {
+    printf("\nGot Ctrl+Z! Exiting safely...\n");
+    exit(0); // End the program
 }
 
 int main() {
-    // Register the handler
-    if (signal(SIGINT, handle_sigint) == SIG_ERR) {
-        perror("Error registering signal handler");
-        return 1;
-    }
+    // Connect the signal to our handler
+    signal(SIGTSTP, handle_SIGTSTP);
 
-    printf("Running... Press Ctrl+C to terminate.\n");
+    printf("Running... Press Ctrl+Z to stop it.\n");
 
-    // Loop to simulate continuous processing
+    // Keep printing until stopped
     while (1) {
-        printf("Working...\n");
-        sleep(2);
+        printf("Still Running...\n");
+        sleep(1);
     }
 
     return 0;
